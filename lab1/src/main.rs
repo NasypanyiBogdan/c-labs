5f7c7dec9fc79b608a65f73c1f69ffbdac823d3e
Nasypanyi Bogdan <nasipanijbogdan@gmail.com>
2026-01-04 02:39:52 +0200

    4567

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -1,38 +1,28 @@
 fn main() {
-    let k = 10; 
-    let n_fixed = 5; 
-    let eps = 0.0001; 
-    let a = 0.0;
-    let b = 1.0;
-    let h = (b - a) / ((k - 1) as f32);
-
-    println!("{:<5} | {:<8} | {:<10} | {:<10} | {:<10}", "i", "x", "Sn (n=5)", "Se (eps)", "y (exact)");
-    println!("{:-<55}", "");
-
-    for i in 1..k + 1 {
-        let x = a + ((i - 1) as f32) * h;
-        let y_exact = ((1.0 + x.powi(2)) / 2.0) * x.atan() - (x / 2.0);
-        let mut sn = 0.0;
-        for n in 1..n_fixed + 1 {
-            sn += term(x, n);
-        }
-
-        let mut se = 0.0;
-        let mut n = 1;
-        loop {
-            let an = term(x, n);
-            if an.abs() < eps { break; }
-            se += an;
-            n += 1;
-        }
-
-        println!("{:2}    | {:8.5} | {:10.5} | {:10.5} | {:10.5}", i, x, sn, se, y_exact);
+    let mut a: [f64; 35] = [0.0; 35];
+    for i in 0..35 {
+        a[i] = (i + 1) as f64 * 1.5; 
     }
-}
 
-fn term(x: f32, n: i32) -> f32 {
-    let sign = if (n + 1) % 2 == 0 { 1.0 } else { -1.0 };
-    let numerator = x.powi(2 * n + 1);
-    let denominator = (4 * n.pow(2) - 1) as f32;
-    sign * (numerator / denominator)
+    println!("Вихідний масив 'a' (перші 5 елементів): {:?}", &a[..5]);
+
+    let mut sum_squares_a: f64 = 0.0;
+    for &val in a.iter() {
+        sum_squares_a += val * val; 
+    }
+
+    let norm = sum_squares_a.sqrt();
+    println!("Норма масиву n = {:.4}", norm);
+
+    let mut b: [f64; 35] = [0.0; 35];
+    for i in 0..35 {
+        b[i] = a[i] / norm;
+    }
+
+    let mut sum_squares_b: f64 = 0.0;
+    for &val in b.iter() {
+        sum_squares_b += val * val;
+    }
+
+    println!("Сума квадратів елементів масиву 'b' = {:.4}", sum_squares_b);
 }
